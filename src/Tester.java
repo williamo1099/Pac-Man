9d12f0bf2b84793b353038d755c373a6463eb473
William Oktavianus <williamoktavianus1099@gmail.com>
2021-09-03 21:23:49 +0700

    Perubahan set ukuran frame
    Mengubah ukuran frame (board) agar tidak fixed 11x11.

--- src/Tester.java
+++ src/Tester.java
@@ -1,5 +1,6 @@
 
 import java.awt.Dimension;
+import java.util.Scanner;
 import javax.swing.JFrame;
 
 /**
@@ -11,12 +12,21 @@ import javax.swing.JFrame;
 public class Tester {
 
     public static void main(String[] args) {
+        // Get input n and tile length.
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input n : ");
+        int n = scan.nextInt();
+        System.out.print("Input tile length : ");
+        int tileLength = scan.nextInt();
+        int frameSize = n * tileLength;
+        
+        // Set game frame.
         JFrame f = new JFrame("Pac-Man");
-        Game board = new Game(11, 50); // Currently, board size is fixed 11x11.
+        Game board = new Game(n, tileLength);
         board.setFocusable(true);
         f.getContentPane().add("Center", board);
         f.pack();
-        f.setSize(new Dimension(620, 620));
+        f.setSize(new Dimension(frameSize, frameSize + 70));
         f.setVisible(true);
     }
 }
